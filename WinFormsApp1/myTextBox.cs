da6e82f78b4ae36522b37267c65a76e7e95e8119
pm <pm@CM33L63.com>
2021-10-27 17:03:14 -0400

    ...

--- WinFormsApp1/myTextBox.cs
+++ WinFormsApp1/myTextBox.cs
@@ -46,8 +46,11 @@ namespace myControls
         private void setUpEvents()
         {
             _tb.TextChanged += new EventHandler     (on_TextChanged);
-            _tb.MouseHover  += new EventHandler     (on_MouseHover);
+
             _lb.MouseClick  += new MouseEventHandler(on_lbMouseClick);
+            _lb.MouseEnter  += new EventHandler     (on_lbMouseEnter);
+            _lb.MouseLeave  += new EventHandler     (on_lbMouseLeave);
+            _lb.Paint       += new PaintEventHandler(on_lbPaint);
         }
 
         // --------------------------------------------------------------------------------------------------------
@@ -60,10 +63,16 @@ namespace myControls
 
         // --------------------------------------------------------------------------------------------------------
 
-        private void on_MouseHover(object sender, EventArgs e)
+        private void on_lbMouseEnter(object sender, EventArgs e)
         {
-            _lb.BackColor = Color.LightGray;
-            _lb.Visible = true;
+            _lb.ForeColor = Color.Red;
+        }
+
+        // --------------------------------------------------------------------------------------------------------
+
+        private void on_lbMouseLeave(object sender, EventArgs e)
+        {
+            _lb.ForeColor = System.Drawing.Color.LightSlateGray;
         }
 
         // --------------------------------------------------------------------------------------------------------
@@ -72,15 +81,13 @@ namespace myControls
         {
             // 'x' button
             _lb.Visible = false;
-
-            _lb.Width  = _tb.Height - 15;
-            _lb.Height = _tb.Height - 15;
+            _lb.Width  = _tb.Height - 10;
+            _lb.Height = _tb.Height - 10;
             _lb.Left   = _tb.Right - _lb.Width - 2;
             _lb.Top    = _tb.Top + 1;
             _lb.BackColor = _tb.BackColor;
-            //_lb.BackColor = Color.Red;
             _lb.ForeColor = System.Drawing.Color.LightSlateGray;
-            _lb.Text = " ";
+            _lb.Text = "x";
 
             _lb.TextAlign = ContentAlignment.TopCenter;
 
@@ -109,5 +116,17 @@ namespace myControls
         }
 
         // --------------------------------------------------------------------------------------------------------
+
+        private void on_lbPaint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.White, e.ClipRectangle);
+            e.Graphics.DrawRectangle(Pens.DarkRed, e.ClipRectangle.X + 1, e.ClipRectangle.Y + 1, e.ClipRectangle.Width-2, e.ClipRectangle.Height-2);
+
+            StringFormat format = new StringFormat(StringFormatFlags.NoWrap);
+                format.LineAlignment = StringAlignment.Center;
+                format.Alignment     = StringAlignment.Center;
+
+            e.Graphics.DrawString("x", _lb.Font, Brushes.Red, e.ClipRectangle, format);
+        }
     };
 };
